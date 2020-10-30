acbb5fe5ef0f28fbe6cb107fd75860b35a11a0c1
Mod-X <xlautomation8@gmail.com>
2020-10-30 22:55:41 +0530

    Auto commit

---
+++ eclipse-workspace/SeleniumHomeProjects/jars/runFb.bat
@@ -0,0 +1,1 @@
+java -jar C:\bin\fb\AddFriend.jar
--- eclipse-workspace/SeleniumHomeProjects/src/AddFbFriend.java
+++ eclipse-workspace/SeleniumHomeProjects/src/AddFbFriend.java
@@ -16,57 +16,154 @@ import java.text.SimpleDateFormat;
 public class AddFbFriend {
 	private static String logFileName="C:/temp/fbLogger.txt";
 	private static WebDriver driver;
-	private static String addFriendUrl="https://www.facebook.com/?sk=ff";
+	private static String addFrndXpath="//div/span[contains(text(),'')]";
+	private static String loginUrl="https://www.facebook.com/full.stack.10420";
+	private static String addFriendUrl="https://www.facebook.com/friends/";
 	private static int fullCount=0;
 	public static void main(String[] args) throws InterruptedException {
-		String url="https://www.facebook.com/full.stack.10420";
         System.setProperty("webdriver.gecko.driver", "C://bin//geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C://bin//chromedriver.exe");
+        
 
-        driver=new ChromeDriver();
- 	   
-        if(args.length>0 && args[0]=="chrome")
-	        	driver=new FirefoxDriver();
-	     
-	    driver.get(url);
-        driver.findElement(By.id("email")).sendKeys("fullstackprofessionals@gmail.com");
+      //  driver=new ChromeDriver();
+       
 
-        driver.findElement(By.id("pass")).sendKeys("Fullstack123");
-        driver.findElement(By.id("loginbutton")).click();
-       for(int j=0;j<5;j++)
-       {
-    	   try{fullCount=fullCount+addFriends();}
-    	   catch(Exception e)
-    	   {}
-    	   appendStrToFile(logFileName,System.lineSeparator()+fullCount+" Friends Added ");
-    	   
-       }
+        if(true)//args.length>0 && args[0]=="chrome")
+	        	driver=new ChromeDriver();
+        else
+        	 	driver=new FirefoxDriver();
+    	System.out.println("First Login Attempt");
+    	
+    	
+        if(!login())
+        {
+        	System.out.println("Second Login Attempt");
+        	login();
+        }
         	
-        System.out.println("finished executing");
-        Thread.sleep(30000);
+        	
+        
+        
+       System.out.println("\nAdding Friends: ");
+       for(int j=0;j<50;j++)
+       {int current=addFriends();
+    	  fullCount=fullCount+current;
+    	//  System.out.println("\nLoop "+j+" complete : " +current+"\tTotal="+fullCount+" Friends Added ");
 
+       }
+       appendStrToFile(logFileName,fullCount+" Friends Added ");
+        	
+        System.out.println("\n\nFinished Executing\n"+fullCount+" Friends Added ");
+        Thread.sleep(30000);
         driver.quit();
 	}
 
+	private static boolean login() {
+		System.out.println("Logging In");
+		boolean status=false;
+		driver.get(loginUrl);
+        driver.findElement(By.id("email")).sendKeys("fullstackprofessionals@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("Fullstack123");
+        driver.findElement(By.id("loginbutton")).click();
+        
+	        try {Thread.sleep(2000);
+	        	
+	        	if(driver.findElements(By.id("email")).size()==0)
+	        		status=true;
+	        	 }
+	        catch(Exception e)
+	        {
+	        	status= false;
+	        }
+	        System.out.println("Login Status: "+status);
+	        return status;
+	}
+
 	private static int addFriends() throws InterruptedException {
+		int localCount=0;String label="";
+
+		driver.navigate().to(addFriendUrl);
+		Thread.sleep(1999);
+		List<WebElement> elements=driver.findElements(By.xpath(addFrndXpath));
+        //System.out.println("\nAdding Friends: ");
+		//System.out.println("Found buttons:"+elements.size());
+        for(int i=0;i<elements.size();i++)
+          {
+        	label=elements.get(i).getText();
+        	if(label.contains("Add Friend"))
+        		{
+        			elements.get(i).click();
+        			System.out.print("+");
+        			localCount++;
+        			break;
+        			
+        		}
+        	
+          }
+//        for(int i=0;i<elements.size();i++)
+//        {	  
+// 		   try{
+// 			  label=elements.get(i).getText();
+// 		   }catch (Exception E){
+// 			   System.out.println("unable to fetch label reloading");
+// 			  driver.navigate().to(addFriendUrl);
+// 			  elements=driver.findElements(By.tagName("button"));
+//
+// 		  }
+// 		  
+// 		if(label.contains("Add Friend") )
+//        	{	boolean success=true;
+//        		   
+//        		   try{elements.get(i).click();}
+//            	   catch(Exception e) { System.out.print(".");success=false;driver.get(addFriendUrl);}
+//        		   
+//        		   if(success) {
+//        			   localCount++;System.out.print("+");
+//        		   }
+//        		   else
+//        			   System.out.print(".");
+//        			   
+//        		   Thread.sleep(1000);
+//            }	
+//        }
+        
+        return localCount;
+}
+	
+	
+	
+	
+	private static int addFriends1() throws InterruptedException {
+			int localCount=0;String label="";
 			driver.navigate().to(addFriendUrl);
-		 	int localCount=0;
-	        List<WebElement> elements=driver.findElements(By.tagName("button"));
+		 	List<WebElement> elements=driver.findElements(By.xpath(addFrndXpath));
 	        System.out.println("\nAdding Friends: ");
 	        for(int i=0;i<elements.size();i++)
-	        {	
-	        	String label=elements.get(i).getText();
-	        	if(label.contains("Add Friend") && localCount<10)
+	        {	  
+     		   try{
+     			  label=elements.get(i).getText();
+     		   }catch (Exception E){
+     			   System.out.println("unable to fetch label reloading");
+     			  driver.navigate().to(addFriendUrl);
+     			  elements=driver.findElements(By.xpath(addFrndXpath));
+
+     		  }
+     		 System.out.println(label);
+     		if(label.contains("Add Friend") )
 	        	{	boolean success=true;
-	        		   System.out.print("+");
-	        		   try{elements.get(i).click();}
-	            	   catch(Exception e) {success=false;driver.get(addFriendUrl);}
 	        		   
-	        		   if(success)localCount++;
+	        		   try{elements.get(i).click();}
+	            	   catch(Exception e) { System.out.print(".");success=false;driver.get(addFriendUrl);}
+	        		   
+	        		   if(success) {
+	        			   localCount++;System.out.print("+");
+	        		   }
+	        		   else
+	        			   System.out.print(".");
+	        			   
 	        		   Thread.sleep(1000);
 	            }	
 	        }
-//	        System.out.println(System.lineSeparator()+localCount+" Friends Added\n ");
 	        
 	        return localCount;
 	}
@@ -76,7 +173,7 @@ public static void appendStrToFile(String fileName,
 { 	
 SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-System.out.println();
+//System.out.println();
 try { 
 
 // Open given file in append mode. 
@@ -84,12 +181,11 @@ BufferedWriter out = new BufferedWriter(
 	
         
 new FileWriter(fileName, true)); 
-out.write(timestamp+" :>>> "+str); 
+out.write(System.lineSeparator()+timestamp+" :>>> "+str); 
 out.close(); 
 } 
 catch (IOException e) { 
 System.out.println("exception occoured" + e); 
 } 
 } 
-
 }
